fb6903adafec900ee80916ffd8def45b5b7b4672
kmduuu <happykimdw@naver.com>
2024-03-15 00:44:08 +0900

    2024.03.15 Chapter2 보이는학생 BufferedReader 사용해서 풀이 완료

--- javaTestPractice/src/com/java/practice/sequenceForStudentHeight/Main.java
+++ javaTestPractice/src/com/java/practice/sequenceForStudentHeight/Main.java
@@ -1,15 +1,31 @@
-package com.java.practice.sequenceForStudentHeight;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+// package com.java.practice.sequenceForStudentHeight;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		String input = br.readLine();
+		int num = Integer.parseInt(input);
+		String height[] = br.readLine().split(" ");
+		int heightArray[] = new int[num];
 		
 
+		for(int i = 0; i < num; i++){
+			heightArray[i] = Integer.parseInt(height[i]);
+		}
+
+		int answer = 1;
+		int max = heightArray[0];
+
+		for(int i = 1; i < num; i++){
+			if(heightArray[i] > max){
+				max = heightArray[i];
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
     }
 }
